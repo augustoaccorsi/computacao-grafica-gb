1fcbf6aafe437cf1b42ca3d9de4940ccd0b0bfe2
augustoaccorsi <augusto.acorsi@gmail.com>
2019-11-18 21:15:49 -0300

    change backgroud color

--- Race/Race/main.cpp
+++ Race/Race/main.cpp
@@ -122,7 +122,7 @@ int main() {
 		glfwPollEvents();
 		processInput(window);
 
-		glClearColor(0.6f, 0.6f, 0.6f, 1.0f);
+		glClearColor(0.2f, 0.5f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glm::mat4 model(1);
